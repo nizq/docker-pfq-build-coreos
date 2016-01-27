258d488b1a171f7e2c73a07685e165c51932e045
尼志强 <ni.zhiqiang@gmail.com>
2016-01-27 09:50:29 +0800

    comment out ghc-cross

--- build-docker.sh
+++ build-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker run --name ghc-cross nilcons/ghc-cross
-docker cp ghc-cross:/ghc-7.8.4-x86_64-unknown-linux-musl.tar.xz .
-docker rm ghc-cross
+# docker run --name ghc-cross nilcons/ghc-cross
+# docker cp ghc-cross:/ghc-7.8.4-x86_64-unknown-linux-musl.tar.xz .
+# docker rm ghc-cross
 
 # stole ghc-musl from https://github.com/nilcons/ghc-musl, thanks!
 docker build -t nizq/alpine-ghc-build .
